7224bd95289f2d86138fa02222626a3b4c8e7752
tobi <krisella74@gmail.com>
2021-03-28 00:21:17 +0100

    display greetings from server

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const isProd = process.env.NODE_ENV === "production";
+
 function App() {
+  const [message, setMessage] = useState(null);
+  useEffect(() => {
+    const effect = async () => {
+      console.log({ env: process.env.NODE_ENV });
+      const data = await fetch(
+        isProd ? "https://examin-er.herokuapp.com/" : "http://localhost:4000"
+      ).then((res) => res.json());
+      setMessage(data.message);
+    };
+    effect();
+  }, []);
   return (
     <div className="App">
-      <div>Welcome to Examinar</div>
+      <div>{message || "Loading.."}</div>
     </div>
   );
 }
