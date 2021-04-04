d1a74aad512db8916fa6c154bae2494b3e809d3f
tobi <krisella74@gmail.com>
2021-04-04 22:36:22 +0100

    bare minimum

--- app.js
+++ app.js
@@ -4,11 +4,33 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+const __questions__ = {};
+
 app.get("/", (req, res) => {
   res.json({ message: "welcome to examinar" });
+});
+
+// create a new exam
+app.post("/exam", (req, res) => {
+  const { questions, title, duration } = req.body; // array of questions
+  const id = new Date();
+
+  const exam = { title, questions, duration };
+  __questions__[id] = exam;
+  return res.json({ message: "success", exam });
+});
+
+// retrieve an existing exam
+app.get("/exam/:id", (req, res, next) => {
+  const { id } = req.params;
+  const exam = __questions__[id];
+  if (!exam) {
+    next();
+    return;
+  }
+  return res.json({ message: "success", exam });
 });
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("app is running!");
 });
-
